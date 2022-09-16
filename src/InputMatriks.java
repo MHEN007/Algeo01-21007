b7cb7ccac7d0a9999e31d9313bb86585a8759e44
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-09-16 19:13:18 +0700

    Menu terbentuk. InputMatriks masih salah

---
+++ src/InputMatriks.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class InputMatriks{
+    public static void main(String[] args){
+        ArrayList<Integer> matriks = new ArrayList<Integer>();
+        matriks.add(1);
+        for (int i = 0; i < matriks.size(); i++) {
+            System.out.println(matriks.get(i));
+        }
+    }
+}
---
+++ src/menu.java
@@ -0,0 +1,70 @@
+import java.util. *;
+
+public class menu{
+    public static void main(String[] args){
+        System.out.println("MENU");
+        System.out.println("1. Sistem Persamaan Linear");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks balikan");
+        System.out.println("4. Interpolasi Polinom");
+        System.out.println("5. Interpolasi Bicubic");
+        System.out.println("6. Regresi linear berganda");
+        System.out.println("7. Keluar");
+
+        // input menunya
+        
+        boolean state = true;
+        Scanner in = new Scanner (System.in);
+        while (state){
+            int i;
+            i = in.nextInt();
+            if(i == 1){
+                // masuk sub-menu
+                subMenu(1);
+            }else if(i == 2){
+                //masuk sub menu determinan
+                subMenu(2);
+            }else if(i == 3){
+                //masuk matriks balikan
+                subMenu(3);
+            }else if (i == 4){
+                //masuk interpolasi polinom
+            }else if (i == 5){
+                //masuk interpolasi bicubic
+            }else if (i == 6){
+                //masuk regresi linear berganda
+            }else if (i == 7){
+                state = false;
+            }else{
+                System.out.println("Input tidak valid! Silakan ulangi input");
+            }
+        }
+    }
+
+    public static void subMenu(int i){
+        if (i == 1 || i == 2 || i == 3){
+            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("2. Metode eliminasi Gauss-Jordan");
+            System.out.println("3. Matriks balikan");
+            System.out.println("4. Kaidah Cramer");
+            int j;
+            Scanner in = new Scanner (System.in);
+            j = in.nextInt();
+            /* INI LOOPINGNYA MASIH SALAH TOLONG DIBENERIN */
+            while (j >= 1 || j <= 4){
+                if (j == 1){
+                    // masuk metode elminasi gauss
+                }else if (j == 2){
+                    //masuk metode eliminasi  gauss -jordan
+                }else if (j == 3){
+                    // masuk metode matriks balikan
+                }else if (j == 4){
+                    // masuk metode kaidah cramer
+                }else{
+                    System.out.println("Input tidak valid! Silakan ulangi input");
+                    j = in.nextInt();
+                }
+            }
+        }
+    }
+}
