53aeb8b2d1b7c591f49e18a78d6e26b38b5f6fff
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-09-17 21:45:26 +0700

    Input matriks fixed

--- src/InputMatriks.java
+++ src/InputMatriks.java
@@ -1,10 +1,42 @@
-import java.util.ArrayList;
+/*
+DESKRIPSI : Program input matriks.
+Menerima input baris m dan kolom n. Lalu dibuat matriks dengan ukuran tersebut.
+Kemudian dilakukan input koefisien aij dan koefisien bij dalam bentuk matriks.
+Semua input dilakukan melalui keyboard. Matriks dibuat dengan array dalam array
+*/
+
+import java.util. *;
 public class InputMatriks{
     public static void main(String[] args){
-        ArrayList<Integer> matriks = new ArrayList<Integer>();
-        matriks.add(1);
-        for (int i = 0; i < matriks.size(); i++) {
-            System.out.println(matriks.get(i));
+        Scanner in = new Scanner (System.in);
+        int m,n,i,j;
+        //input m
+        System.out.print("Masukkan ukuran baris matriks (m): ");
+        m = in.nextInt();
+
+        //input n
+        System.out.print("Masukkan ukuran baris matriks (n): ");
+        n = in.nextInt();
+
+        //membuat matriks ukuran m x n
+        int[][] matriks = new int[m][n]; // matriks ukuran m baris n kolom sudah terinisialisasi
+
+        //prosedur memasukkan elemen ke matriks
+        // memasukkan elemen harus dalam satu baris yang dipisahkan oleh satu buah spasi
+        // contoh : 1 2 3
+        for (i = 0; i < m; i++){
+            for (j = 0; j < n; j++){
+                matriks[i][j] = in.nextInt();
+            }
         }
+
+        /*testing cetak matriks
+        for (i = 0; i < m; i++){
+            for (j = 0; j < n; j++){
+                System.out.print(matriks[i][j]+" ");
+            }
+            System.out.println();
+        }
+        */
     }
 }
