fac51a1a3b40409e5fc2ea1a229f45088b37194e
agshaathalla <agshaathalla@gmail.com>
2022-09-29 17:57:32 +0700

    Fungsi Invers dengan OBE

--- src/InversOBE.java
+++ src/InversOBE.java
@@ -1,8 +1,49 @@
 class InversOBE{
     public static void main(String[] args){
-        Matriks m = new Matriks();
-        double[][] matriks = {{4,5,1},{3,4,3},{1,2,1}};
-        double[][] invers = m.InversOBE(matriks, m.getRow(matriks), m.getCol(matriks));
-        m.printMatriks(invers, m.getRow(invers), m.getCol(invers));
+        Matriks matriks = new Matriks();
+
+        int m = matriks.inputRow();
+        int n = matriks.inputCol();
+        double[][] matrix = matriks.makeMatrix(m,n);
+        matriks.inputMatriks(matrix,m,n);
+
+        double[][] copydet = matriks.copyMatriks(matrix,m,n);
+        double[][] identitas = matriks.makeIdentitas(m,n);
+
+        if (matriks.Determinan(copydet,m,n) == 0){
+            System.out.print("Tidak dapat menemukan matriks karena determinan bernilai 0!");
+        }
+
+        else{
+        int o = n*2;
+        double[][] operasi = matriks.makeMatrix(m,o);
+        int i,j,k;
+        //masukkan matriks ditanya ke matriks operasi
+        for (i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                operasi[i][j] = matrix[i][j];
+            }
+        }
+        
+        for(i=0;i<m;i++){
+            for (j=n;j<o;j++){
+                operasi[i][j] = identitas[i][j-n];
+            }
+        }
+                matriks.printMatriks(operasi,m,o);
+
+        matriks.GaussJordan(operasi,m,o);
+        matriks.printMatriks(operasi,m,o);
+
+        double[][] hasil = matriks.makeMatrix(m,n);
+        for(i=0;i<m;i++){
+            for (j=n;j<o;j++){
+                hasil[i][j-n] = operasi[i][j];
+            }
+        }
+
+        matriks.printMatriks(hasil,m,n);
+        }
+
     }
 }
